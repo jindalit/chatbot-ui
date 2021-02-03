ce54d83139849fff8f886450e02588f102bbce29
Mayank Jindal <anup.learn004@gmail.com>
2021-02-03 20:45:56 +0530

    Updates

---
+++ precache-manifest.877a4ab54f0a2f352558b8f8be631bd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c1ff52a846b01708a3ecf2209a77123",
+    "url": "/chatbot-ui/index.html"
+  },
+  {
+    "revision": "ee3d54dd5caff3729d43",
+    "url": "/chatbot-ui/static/css/main.b65d7081.chunk.css"
+  },
+  {
+    "revision": "0eb9965d7be88e897796",
+    "url": "/chatbot-ui/static/js/2.2e8d6aa0.chunk.js"
+  },
+  {
+    "revision": "297e52b030b79766803028eb73cc3fad",
+    "url": "/chatbot-ui/static/js/2.2e8d6aa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee3d54dd5caff3729d43",
+    "url": "/chatbot-ui/static/js/main.d879f818.chunk.js"
+  },
+  {
+    "revision": "13f2d74ab628533332c7",
+    "url": "/chatbot-ui/static/js/runtime-main.e64b7330.js"
+  }
+]);
